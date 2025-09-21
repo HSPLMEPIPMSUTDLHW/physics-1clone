83ac25482b86fae9f612b6f73c02ff62c209b661
yeah <72712916+HSPLMEPIPMSUTDLHW@users.noreply.github.com>
2025-09-21 18:53:27 -0400

    fixed ui
    fixed the sliders and their labels

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -55,15 +55,15 @@ void draw()
 {
     BeginDrawing();
     ClearBackground(DARKBLUE);
-    GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Angle", TextFormat("%.1f", launchAngle), &launchAngle, -PI, PI);
-    GuiSliderBar(Rectangle{ 60, 20, 1000, 10 }, "X Pos", TextFormat("%.1f", X), &X, 0, InitialWidth);
-    GuiSliderBar(Rectangle{ 60, 35, 1000, 10 }, "Y Pos", TextFormat("%.1f", Y), &Y, 0, InitialHeight);
-    GuiSliderBar(Rectangle{ 60, 50, 1000, 10 }, "Speed", TextFormat("%.1f", launchSpeed), &launchSpeed, 0, 500);
+    GuiSliderBar(Rectangle{ 60, 10, 1000, 10 }, "Angle", TextFormat("%.1f", launchAngle), &launchAngle, -PI, PI);
+    GuiSliderBar(Rectangle{ 60, 40, 1000, 10 }, "X Pos", TextFormat("%.1f", X), &X, 0, InitialWidth);
+    GuiSliderBar(Rectangle{ 60, 70, 1000, 10 }, "Y Pos", TextFormat("%.1f", Y), &Y, 0, InitialHeight);
+    GuiSliderBar(Rectangle{ 60, 100, 1000, 10 }, "Speed", TextFormat("%.1f", launchSpeed), &launchSpeed, 0, 500);
    
     DrawText(TextFormat("Angle: %.1f", -launchAngle * (180 / PI)), GetScreenWidth() - 140, 10, 20, LIGHTGRAY);;
     DrawText(TextFormat("X: %.1f", birdPos.x), GetScreenWidth() - 140, 40, 20, LIGHTGRAY);
     DrawText(TextFormat("Y: %.1f", birdPos.y), GetScreenWidth() - 140, 70, 20, LIGHTGRAY);
-    DrawText(TextFormat("X: %.1f", birdPos.x), GetScreenWidth() - 140, 40, 20, LIGHTGRAY);
+    DrawText(TextFormat("Speed: %.1f", launchSpeed), GetScreenWidth() - 140, 100, 20, LIGHTGRAY);
    // DrawText(TextFormat("Hyp: %.f", Hyp(birdPos.x, birdPos.y)), GetScreenWidth() - 140, 70, 20, LIGHTGRAY);
   
   

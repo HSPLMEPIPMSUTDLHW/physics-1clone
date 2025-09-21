abb7ef52aa2bb65395b27a2a83a69528912de47a
yeah <72712916+HSPLMEPIPMSUTDLHW@users.noreply.github.com>
2025-09-21 18:46:32 -0400

    Finished Lab 2
    Finished Lab 2. Has sliders at the top of the screen to change the position, launch speed, and launch angle

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -16,9 +16,10 @@ float startX = 500;
 float startY = 500;
 float X = 500;
 float Y = 500;
-float frequency = 1;
-float amplitude = 100;
+//float frequency = 1;
+//float amplitude = 100;
 float launchAngle;
+float launchSpeed;
 Vector2 birdPos;
  
 
@@ -26,24 +27,16 @@ void update()
 {
     dt = 1.0f / TARGET_FPS;
     time += dt;
-    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+    if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
     {          
-     //   X = GetMousePosition().x;
-   //     Y = GetMousePosition().y;
-      //  launchAngle
-    }
-    else
-    {
-  
- //       X = startX;
-   //     Y = startY;
+        launchAngle = 0;
+        X = startX;
+        Y = startY;
+        launchSpeed = 0;
     }
     birdPos.x = startX - X;
     birdPos.y = startY - Y;
-   // launchAngle = atan((birdPos.y / birdPos.x));
-     launchAngle = atan((birdPos.y / birdPos.x));
- //   X = X + (-sin(time * frequency)) * frequency * amplitude * dt;
-//    Y = Y + (cos(time * frequency)) * frequency * amplitude * dt;
+
 
 
 }
@@ -51,29 +44,36 @@ float Hyp(float x, float y)
 {
     return sqrt((x * x) + (y * y));
 }
+Vector2 launchVel(float X, float Y,float angle, float hyp)
+{
+    float x = cos(angle) * hyp;
+    float y = sin(angle) * hyp;
+   
+    return { X+x,Y+y };
+}
 void draw()
 {
     BeginDrawing();
     ClearBackground(DARKBLUE);
+    GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Angle", TextFormat("%.1f", launchAngle), &launchAngle, -PI, PI);
+    GuiSliderBar(Rectangle{ 60, 20, 1000, 10 }, "X Pos", TextFormat("%.1f", X), &X, 0, InitialWidth);
+    GuiSliderBar(Rectangle{ 60, 35, 1000, 10 }, "Y Pos", TextFormat("%.1f", Y), &Y, 0, InitialHeight);
+    GuiSliderBar(Rectangle{ 60, 50, 1000, 10 }, "Speed", TextFormat("%.1f", launchSpeed), &launchSpeed, 0, 500);
    
-
-    
-    GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
-    GuiSliderBar(Rectangle{ 60, 20, 1000, 10 }, "Angle", TextFormat("%.2f", launchAngle), &launchAngle, -PI, PI);
-    GuiSliderBar(Rectangle{ 60, 35, 1000, 10 }, "X Pos", TextFormat("%.2f", X), &X, 0, InitialWidth);
-    GuiSliderBar(Rectangle{ 60, 50, 1000, 10 }, "Y Pos", TextFormat("%.2f", Y), &Y, 0, InitialHeight);
-    GuiSliderBar(Rectangle{ 60, 65, 1000, 10 }, "Speed", TextFormat("%.2f", Y), &Y, -200, 200);
-    //Postion is based on the starting postion
-    DrawText(TextFormat("X: %.f", birdPos.x), GetScreenWidth() - 140, 10, 20, LIGHTGRAY);
-    DrawText(TextFormat("Y: %.f", birdPos.y), GetScreenWidth() - 140, 40, 20, LIGHTGRAY);
-    DrawText(TextFormat("Hyp: %.f", Hyp(birdPos.x, birdPos.y)), GetScreenWidth() - 140, 70, 20, LIGHTGRAY);
- //   DrawText(TextFormat("X: %.f", X), GetScreenWidth() - 140, 100, 20, LIGHTGRAY);
-    DrawText(TextFormat("Angle: %.f", launchAngle*(180/PI)), GetScreenWidth() - 140, 130, 20, LIGHTGRAY);
-    //sin
+    DrawText(TextFormat("Angle: %.1f", -launchAngle * (180 / PI)), GetScreenWidth() - 140, 10, 20, LIGHTGRAY);;
+    DrawText(TextFormat("X: %.1f", birdPos.x), GetScreenWidth() - 140, 40, 20, LIGHTGRAY);
+    DrawText(TextFormat("Y: %.1f", birdPos.y), GetScreenWidth() - 140, 70, 20, LIGHTGRAY);
+    DrawText(TextFormat("X: %.1f", birdPos.x), GetScreenWidth() - 140, 40, 20, LIGHTGRAY);
+   // DrawText(TextFormat("Hyp: %.f", Hyp(birdPos.x, birdPos.y)), GetScreenWidth() - 140, 70, 20, LIGHTGRAY);
+  
+  
+      
     DrawCircle(X, Y, 25, RED);
-   // DrawCircle(startX,startY, 5, GREEN);
-    //DrawLineV({ X,Y }, { startX,startY }, WHITE);
-  //  DrawLineV({ startX,startY }, { startX +birdPos.x,startY + birdPos.y }, RED);
+    //marks starting position
+    DrawCircle(startX, startY, 5, GREEN);
+ 
+   
+    DrawLineV({X,Y}, launchVel(X,Y,launchAngle, launchSpeed), RED);
     EndDrawing();
 }
 int main()
